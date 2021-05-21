ab3317304d0d9d2a6a462c657a563760dff0def9
dahuilang <279437541@qq.com>
2021-05-21 10:12:10 +0800

    ·

---
+++ openwrt-21.02.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+rm -rf ./*/.git
+rm -rf ./*/.svn
+rm -rf ./*/*/.svn
+rm -rf ./*/*/.git
+
+rm -rf ./ReadMe.md
+rm -rf ./*/ReadMe.md
+rm -rf ./*/README.md
+rm -rf ./*/LICENSE
+rm -rf .svn
+rm -rf ./*/readme.txt
+rm -f .gitattributes .gitignore
+exit 0
