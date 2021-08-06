356368f4b0aef7019cc1799f7f926882ce732c9c
shidahuilang <jmkongdeshuai@gmail.com>
2021-08-06 19:51:58 +0800

    ·

---
+++ openwrt-21.02.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+svn co https://github.com/281677160/openwrt-package/trunk ./
+
+rm -rf ./*/.git
+rm -rf ./*/.svn
+rm -rf ./*/*/.svn
+rm -rf ./*/*/.git
+
+rm -rf ./ReadMe.md
+rm -rf ./*/ReadMe.md
+rm -rf ./*/README.md
+rm -rf ./*/LICENSE
+rm -rf .svn
+rm -rf ./*/readme.txt
+rm -f .gitattributes .gitignore
+exit 0
