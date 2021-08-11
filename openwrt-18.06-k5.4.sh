d629c443c3b2b1fe35d4be398213aa08f9fb82f0
shidahuilang <81285256+shidahuilang@users.noreply.github.com>
2021-08-11 21:38:58 +0800

    Update openwrt-18.06-k5.4.sh

--- openwrt-18.06-k5.4.sh
+++ openwrt-18.06-k5.4.sh
@@ -1,2 +1,13 @@
 svn co https://github.com/immortalwrt/packages/trunk ./
 
+rm -rf ./*/.git
+rm -rf ./*/.svn
+rm -rf ./*/*/.svn
+rm -rf ./*/*/.git
+rm -rf ./.github
+rm -rf ./*/README.md
+rm -rf ./*/LICENSE
+rm -rf .svn
+rm -rf ./*/readme.txt
+rm -f .gitattributes .gitignore
+exit 0
