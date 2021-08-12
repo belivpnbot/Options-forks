2e1ef7218029651c1b1acce8a820f06847a5a531
shidahuilang <81285256+shidahuilang@users.noreply.github.com>
2021-08-12 11:45:01 +0800

    Update lede-17.01.sh

--- lede-17.01.sh
+++ lede-17.01.sh
@@ -132,7 +132,7 @@ EOF
 
 chmod -R 775 feeds/luci/applications/luci-app-rebootschedule
 # 判断变量值，如果有效发送微信通知
-if [ -n "$FOLDERS" ]; then  curl https://sc.ftqq.com/$SCT18103TNKBuiwMdcCJK5GWIM7mloVdX.send?text=$FOLDERSX--同步失败; fi
+if [ -n "$FOLDERS" ]; then  curl https://sc.ftqq.com/$SCKEY.send?text=$FOLDERSX--同步失败; fi
 
 #TG通知
 if [ -n "$FOLDERS" ]; then  curl "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" -d "chat_id=$TELEGRAM_CHAT_ID&text=🚫源码同步失败，分支：Package_$matrix_target，失败列表：$FOLDERSX......"; else curl "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" -d "chat_id=$TELEGRAM_CHAT_ID&text=🎉源码同步成功，分支：Package_$matrix_target......"; fi
