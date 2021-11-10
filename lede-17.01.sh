d58cd20d5217098cff15fc6e3bad85da8b4d8d89
shidahuilang <81285256+shidahuilang@users.noreply.github.com>
2021-11-10 15:44:43 +0800

    Update lede-17.01.sh

--- lede-17.01.sh
+++ lede-17.01.sh
@@ -103,7 +103,7 @@ svn co https://github.com/281677160/AdGuardHome/trunk/adguardhome feeds/packages
 svn co https://github.com/281677160/AdGuardHome/trunk/packr feeds/packages/devel/packr
 
 # ddnsto插件
-git clone https://github.com/linkease/nas-packages-luci/trunk/luci/luci-app-ddnsto package/luci-app-ddnsto
+git clone https://github.com/shidahuilang/luci-app-ddnsto feeds/luci/applications/luci-app-ddnsto
 
 
 # 增加filebrowser的用户名跟密码提示
