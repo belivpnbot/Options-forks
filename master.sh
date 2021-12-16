77ca52d31675fca017b484b9a78e65b978494d67
shidahuilang <81285256+shidahuilang@users.noreply.github.com>
2021-12-16 18:56:51 +0800

    Update master.sh

--- master.sh
+++ master.sh
@@ -97,7 +97,7 @@ rm -rf feeds/packages/net/luci-app-openclash
 rm -rf feeds/packages/net/img
 svn co https://github.com/frainzy1477/luci-app-clash/trunk feeds/luci/applications/luci-app-clash
 svn co https://github.com/shidahuilang/OpenClash feeds/luci/applications/luci-app-openclash
-svn co https://github.com/shidahuilang/OpenClash/trunk feeds/packages/net
+svn co https://github.com/shidahuilang/OpenClash feeds/packages/net
 
 
 # luci-theme主题插件
